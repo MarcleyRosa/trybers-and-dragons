6b55375e99cde99b812838f3da88dfc0f29e4bed
Marcley <marcleyrosa@gmail.com>
2023-01-19 15:59:27 -0300

    Requisito 04 Archetypes

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,28 @@
+export default abstract class Archetype {
+  private static inst = 0;
+  constructor(
+    private _name: string,
+    private _special = 0,
+    private _cost = 0,
+  ) {
+    Archetype.inst += 1;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get special() {
+    return this._special;
+  }
+
+  get cost() {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances() {
+    return Archetype.inst;
+  }
+
+  abstract get energyType(): string;
+}
