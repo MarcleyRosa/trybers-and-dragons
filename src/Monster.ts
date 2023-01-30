e8a4098800f452ac0b74d1e73787a12ddfa3f025
Marcley <marcleyrosa@gmail.com>
2023-01-30 20:54:19 -0300

    Requisito 09 class Monster

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,6 +1,25 @@
-// import { SimpleFighter } from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 
-// export default class Monster implements SimpleFighter {
-//   private lifePoints: number;
-//   private strength: number;
-// }
+export default class Monster implements SimpleFighter {
+  constructor(private _lifePoints = 85, private _strength = 63) {
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this._lifePoints -= attackPoints;
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter | Fighter): void {
+    const attck = this._strength;
+    enemy.receiveDamage(attck);
+  }
+}
