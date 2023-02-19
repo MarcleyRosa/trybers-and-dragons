ed24c125900e8ae1a9c2fe26bdff7ee2b87e589c
Marcley <marcleyrosa@gmail.com>
2023-02-19 08:00:55 -0300

    chore: requisito 13

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,13 @@ import Character from './Character';
 import Dragon from './Dragon';
 import Monster from './Monster';
 
-const player1 = new Character('Arthorius');
-const player2 = new Character('Konsumerat');
-const player3 = new Character('Reward');
+const player1 = new Character('player1');
+const player2 = new Character('player2');
+const player3 = new Character('player3');
 
-player1.levelUp();
-player1.levelUp();
-player1.levelUp();
-player1.levelUp();
-player1.levelUp();
-player1.levelUp();
-player1.levelUp();
-player1.levelUp();
+for (let index = 0; index <= 15; index += 1) {
+  player1.levelUp();
+}
 
 const monster1 = new Monster();
 const monster2 = new Dragon();
